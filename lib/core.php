0d43e00d6caa0e9b4addf1da655794702b2792e5
Norcross <norcross@reaktivstudios.com>
2015-05-13 10:31:37 -0400

    rebuilt constructor call and added filters for various formatting and display

--- lib/core.php
+++ lib/core.php
@@ -14,10 +14,19 @@ class SLLD_Core
 {
 
 	/**
-	 * [__construct description]
+	 * call the loader.
 	 */
-	public function __construct() {
-		add_filter( 'login_redirect',                   array( $this, 'store_user_login_time'   ),  10, 3   );
+	public function init() {
+		add_action( 'wp_loaded',                        array( $this, 'loader'                  )           );
+	}
+
+	/**
+	 * the actual loader function. there are many like it, but this one is mine
+	 *
+	 * @return void
+	 */
+	public function loader() {
+		add_filter( 'login_redirect',                   array( $this, 'store_login_timestamp'   ),  10, 3   );
 		add_action( 'personal_options',                 array( $this, 'show_login_on_profile'   )           );
 		add_filter( 'pre_user_query',                   array( $this, 'last_login_sort_vars'    )           );
 		add_action( 'manage_users_custom_column',       array( $this, 'user_column_data'        ),  10, 3   );
@@ -26,22 +35,22 @@ class SLLD_Core
 	}
 
 	/**
-	 * set the time of login for a user before proceeding
+	 * store the login timestamp for a user before proceeding
 	 *
 	 * @param  [type] $redirect_to [description]
 	 * @param  [type] $request     [description]
 	 * @param  [type] $user        [description]
 	 * @return [type]              [description]
 	 */
-	public function store_user_login_time( $redirect_to, $request, $user ) {
+	public function store_login_timestamp( $redirect_to, $request, $user ) {
 
 		// bail on invalid user
 		if ( empty( $user ) || ! is_object( $user ) || is_wp_error( $user ) ) {
 			return $redirect_to;
 		}
 
-		// set the login time
-		update_user_meta( $user->ID, '_slld_last_login', current_time( 'timestamp' ) );
+		// set the login timestamp
+		self::set_login_timestamp( $user->ID, current_time( 'timestamp' ) );
 
 		// now continue as normal
 		return $redirect_to;
@@ -55,11 +64,22 @@ class SLLD_Core
 	 */
 	public function show_login_on_profile( $user ) {
 
+		// bail on missing or invalid user
+		if ( empty( $user ) || ! is_object( $user ) || empty( $user->ID ) ) {
+			return;
+		}
+
 		// get the login time
-		$login  = get_user_meta( $user->ID, '_slld_last_login', true );
+		$login  = self::get_login_timestamp( $user->ID );
+
+		// set the formatting type we want to use
+		$type   = apply_filters( 'slld_profile_format_type', 'human', $user->ID, $login );
 
 		// set up display
-		$show   = ! empty( $login ) && absint( $login ) !== absint( 9999999999 ) ? self::format_login_stamp( $login ) : __( 'never', 'store-last-login-date' );
+		$show   = ! empty( $login ) && absint( $login ) !== absint( 9999999999 ) ? self::format_login_stamp( $login, esc_attr( $type ), $user->ID ) : __( 'never', 'store-last-login-date' );
+
+		// filter the display
+		$show   = apply_filters( 'slld_profile_display', $show, $user->ID, $login );
 
 		// echo out the box
 		echo '<tr class="user-last-login-time">';
@@ -122,15 +142,13 @@ class SLLD_Core
 		if ( 'last-login' == $column_name ) {
 
 			// get the login time
-			$login  = get_user_meta( $user_id, '_slld_last_login', true );
+			$login  = self::get_login_timestamp( $user_id );
 
-			// update the last login if missing
-			if ( empty( $login ) ) {
-				self::set_fallback_login_time( $user_id );
-			}
+			// set up the display
+			$show   = ! empty( $login ) && absint( $login ) !== absint( 9999999999 ) ? self::format_login_stamp( $login, 'date', $user_id ) . '<br>' . self::format_login_stamp( $login, 'time', $user_id ) : '<em>' . __( 'never', 'store-last-login-date' ) . '</em>';
 
-			// show it
-			return ! empty( $login ) && absint( $login ) !== absint( 9999999999 ) ? self::format_login_stamp( $login, 'date' ) . '<br>' . self::format_login_stamp( $login, 'time' ) : '<em>' . __( 'never', 'store-last-login-date' ) . '</em>';
+			// filter the display
+			return apply_filters( 'slld_column_display', $show, $user_id, $login );
 		}
 
 		// return the column value
@@ -156,6 +174,7 @@ class SLLD_Core
 	 * set the last login and cohort group to be sortable
 	 *
 	 * @param  [type] $columns [description]
+	 *
 	 * @return [type]          [description]
 	 */
 	public function make_user_sortable_cols( $columns ) {
@@ -168,13 +187,34 @@ class SLLD_Core
 	}
 
 	/**
-	 * set a fallback time for a user to make sure all
-	 * the sorting logic works as expected
+	 * get the login timestamp for a user
 	 *
 	 * @param integer $user_id   the user ID
+	 *
+	 * @return integer           the login timestamp
 	 */
-	public static function set_fallback_login_time( $user_id = 0 ) {
-		update_user_meta( $user_id, '_slld_last_login', 9999999999 );
+	public static function get_login_timestamp( $user_id = 0 ) {
+
+		// get my time
+		$stamp  = get_user_meta( $user_id, '_slld_last_login', true );
+
+		// if empty, store the fallback
+		if ( empty( $stamp ) ) {
+			self::set_login_timestamp( $user_id );
+		}
+
+		// now return it
+		return ! empty( $stamp ) ? floatval( $stamp ) : 9999999999;
+	}
+
+	/**
+	 * set the login timestamp for a user
+	 *
+	 * @param integer $user_id   the user ID
+	 * @param integer $stamp     the timestamp
+	 */
+	public static function set_login_timestamp( $user_id = 0, $stamp = 9999999999 ) {
+		update_user_meta( $user_id, '_slld_last_login', floatval( $stamp ) );
 	}
 
 	/**
@@ -182,10 +222,11 @@ class SLLD_Core
 	 *
 	 * @param  integer $login    the login stored timestamp
 	 * @param  string  $type     the type of format requested. will take 'human', 'date', and 'time'
+	 * @param integer $user_id   the user ID. only used for optional filtering
 	 *
 	 * @return string            the requested date / time formatted
 	 */
-	public static function format_login_stamp( $login = 0, $type = 'human' ) {
+	public static function format_login_stamp( $login = 0, $type = 'human', $user_id = 0 ) {
 
 		// if no type is provided, just return the login timestamp
 		if ( empty( $type ) ) {
@@ -198,14 +239,17 @@ class SLLD_Core
 			// human diff
 			case 'human':
 
-				return human_time_diff( floatval( $login ) ) . ' ' . __( 'ago', 'store-last-login-date' );
+				return human_time_diff( floatval( $login ), current_time( 'timestamp' ) ) . ' ' . __( 'ago', 'store-last-login-date' );
 				break;
 
 			// date
 			case 'date':
 
 				// get my date format with a fallback
-				$format = get_option( 'date_format', 'm/d/Y' );
+				$format = get_option( 'date_format', 'F j, Y' );
+
+				// pass it through a filter
+				$format = apply_filters( 'slld_date_format', $format, $user_id );
 
 				// return the formatted date
 				return date( $format, floatval( $login ) );
@@ -215,8 +259,12 @@ class SLLD_Core
 			case 'time':
 
 				// get my time format with a fallback
-				$format = get_option( 'time_format', 'g:ia' );
+				$format = get_option( 'time_format', 'g:i a' );
 
+				// pass it through a filter
+				$format = apply_filters( 'slld_time_format', $format, $user_id );
+
+				// return the formatted date
 				return date( $format, floatval( $login ) );
 				break;
 		}
@@ -228,7 +276,8 @@ class SLLD_Core
 // end exists check
 }
 
-
 // Instantiate our class
-new SLLD_Core();
+$SLLD_Core = new SLLD_Core();
+$SLLD_Core->init();
+
 
